327265d7267c33c00ec1d9b977346362e85720f6
Ximin Luo <infinity0@gmx.com>
2011-07-28 01:13:56 +0100

    rm unnecessary includes from classpath - don't include CPStringBuilder, it's only used in non-native branch and can be replaced with StringBuilder - don't build separate libjcl.so, instead bundle it into libgmp-jni.so - set PHONY targets - better variable names

--- Test.java
+++ Test.java
@@ -4,9 +4,9 @@ public class Test {
 	public static void main(String[] args) {
 		Boolean debug = Boolean.getBoolean("gnu.native.debug");
 		if (!debug) {
-			System.out.println("please re-run this with -Dgnu.native.debug=true");
+			System.err.println("please re-run this with -Dgnu.native.debug=true");
 		} else {
-			System.out.println("if you didn't see \"using pure java implementation\", all is well");
+			System.out.println("if you don't see \"using pure java implementation\", all is well");
 		}
 		BigInteger a = new BigInteger("999999999999999999999999999999999999999999999999999999999999999999");
 		BigInteger b = new BigInteger("999999999999999999999999999999999999999999999999999999999999999998");
@@ -18,4 +18,3 @@ public class Test {
 		System.out.println("test passed");
 	}
 }
-
